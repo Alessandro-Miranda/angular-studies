e2d62b8f160568a4d0c7a1adebee78689369d776
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2024-07-14 00:05:35 -0300

    finalizar implementação de validações no form template-driven

--- contact-form-template-driven/src/app/app.component.ts
+++ contact-form-template-driven/src/app/app.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'contact-form';
+  title = 'template-driven-form';
+  name = {
+    firstName: '',
+    lastName: '',
+  }
+  email = '';
+  queryType = '';
+  message = '';
+  accepted = false;
+
+  onSubmit() {
+    console.log(`Seu nome é ${this.name.firstName} ${this.name.lastName}, seu e-mail é: ${this.email} a query type selecionada foi ${this.queryType} e vc digitou a mensagem ${this.message} e vc escolheu ${this.accepted} para consentimento`);
+  }
 }
